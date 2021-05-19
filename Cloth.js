9526b2e55a75a14398690ae9ed74f982faf9e132
Michael Romay <amerror@gmail.com>
2021-05-18 18:56:53 -0700

    pose and cloth on same screen

--- Cloth.js
+++ Cloth.js
@@ -1,3 +1,265 @@
+let video;
+let poseNet;
+let poses = [];
+let pose;
+let right;
+let left;
+let scoreLeft;
+let scoreRight;
+// let nose;
+// let leftEye;
+// let rightEye;
+
+function setup() {
+  createCanvas(640, 480);
+  video = createCapture(VIDEO);
+  video.size(width, height);
+
+  // Create a new poseNet method with a single detection
+  poseNet = ml5.poseNet(video, modelReady);
+  // This sets up an event that fills the global variable "poses"
+  // with an array every time new poses are detected
+  poseNet.on("pose", function(results) {
+    poses = results;
+  });
+  // Hide the video element, and just show the canvas
+  video.hide();
+}
+
+function modelReady() {
+  select("#status").html("Model Loaded");
+}
+
+function draw() {
+  image(video, 0, 0, width, height);
+
+  // We can call both functions to draw all keypoints and the skeletons
+  drawKeypoints();
+  // drawSkeleton();
+  
+  // pose = poses[0].pose;
+  // left = pose['leftWrist'];
+  // right = pose['rightWrist'];
+  // scoreRight = poses[0].pose.keypoints[10].score;
+  // scoreLeft = poses[0].pose.keypoints[9].score;
+
+  // if(poses.length > 0) {
+  //   console.log('leftWrist: ', poses[0].pose.keypoints[9].position.x);
+  // }
+  
+  // console.log('left wrist: ', left);
+  // console.log('right wrist: ', right);
+  // console.log('score left: ', scoreLeft);
+  // console.log('score right: ', scoreRight);
+}
+
+// A function to draw ellipses over the detected keypoints
+function drawKeypoints() {
+  // Loop through all the poses detected
+  for (let i = 0; i < poses.length; i += 1) {
+    // For each pose detected, loop through all the keypoints
+    const pose = poses[i].pose;
+    // const pose = poses[i].pose['leftWrist'];
+    // const leftWrist = poses[i].pose['leftWrist'];
+    for (let j = 0; j < pose.keypoints.length; j += 1) {
+      // A keypoint is an object describing a body part (like rightArm or leftShoulder)
+      const keypoint = pose.keypoints[j];
+      // Only draw an ellipse is the pose probability is bigger than 0.2
+      if (keypoint.score > 0.2) {
+        fill(255, 0, 0);
+        noStroke();
+        ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
+        // console.log(keypoint.position.x, keypoint.position.y)
+      }
+    }
+  }
+}
+
+// A function to draw the skeletons
+// function drawSkeleton() {
+//   // Loop through all the skeletons detected
+//   for (let i = 0; i < poses.length; i += 1) {
+//     const skeleton = poses[i].skeleton;
+//     // For every skeleton, loop through all body connections
+//     for (let j = 0; j < skeleton.length; j += 1) {
+//       const partA = skeleton[j][0];
+//       const partB = skeleton[j][1];
+//       stroke(255, 0, 0);
+//       line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
+//     }
+//   }
+// }
+
+/////////////////
+
+function dragTracker(options) {
+  "use strict";
+  
+  //Element.closest polyfill:
+  //https://developer.mozilla.org/en-US/docs/Web/API/Element/closest
+  const ep = Element.prototype;
+  if (!ep.matches)
+       ep.matches = ep.msMatchesSelector || ep.webkitMatchesSelector;
+  if (!ep.closest)
+       ep.closest = function(s) {
+          var node = this;
+          do {
+              if(node.matches(s)) return node;
+              //https://github.com/Financial-Times/polyfill-service/issues/1279
+              node = (node.tagName === 'svg') ? node.parentNode : node.parentElement;
+          } while(node); 
+
+          return null;
+      };
+
+
+  options = options || {};
+  const container = options.container || document.documentElement,
+        callback = options.callback || console.log,
+        callbackStart = options.callbackDragStart,
+        callbackEnd = options.callbackDragEnd,
+        selector = options.selector,
+        //handleOffset: "center", true (default), false
+        handleOffset = options.handleOffset || (options.handleOffset !== false),
+        roundCoords = (options.roundCoords !== false),
+        dragOutside = (options.dragOutside !== false)
+  ;
+  //Whether callback coordinates should be the dragged element's center instead of the top-left corner
+  let offsetToCenter = null;
+  switch(handleOffset) {
+      case 'center':
+          offsetToCenter = true; break;
+      case 'topleft':
+      case 'top-left':
+          offsetToCenter = false; break;
+  }
+
+
+  let dragged, mouseOffset, dragStart;
+  
+  function getMousePos(e, elm, offset, stayWithin) {
+      // let x = e.clientX,
+      //     y = e.clientY;
+
+      let x = e.poses[0].pose.keypoints[9].position.x,
+          y = e.poses[0].pose.keypoints[9].position.y;
+
+      function respectBounds(value, min, max) {
+          return Math.max(min, Math.min(value, max));
+      }
+
+      if(elm) {
+          const bounds = elm.getBoundingClientRect();
+          x -= bounds.left;
+          y -= bounds.top;
+
+          if(offset) {
+              x -= offset[0];
+              y -= offset[1];
+          }
+          if(stayWithin) {
+              x = respectBounds(x, 0, bounds.width);
+              y = respectBounds(y, 0, bounds.height);
+          }
+
+          //Adjust the mouseOffset on the dragged element
+          //if the element is positioned by its center:
+          if(elm !== container) {
+              const center = (offsetToCenter !== null)
+                  ? offsetToCenter
+                  //SVG circles and ellipses are positioned by their center (cx/cy), not the top-left corner:
+                  : (elm.nodeName === 'circle') || (elm.nodeName === 'ellipse');
+
+              if(center) {
+                  x -= bounds.width/2;
+                  y -= bounds.height/2;
+              }
+          }
+      }
+      return (roundCoords ? [Math.round(x), Math.round(y)] : [x, y]);
+  }
+
+  function onDown(e) {
+      dragged = selector ? e.target.closest(selector) : {};
+      if(dragged) {
+          e.preventDefault();
+
+          mouseOffset = (selector && handleOffset) ? getMousePos(e, dragged) : [0, 0];
+          dragStart = getMousePos(e, container, mouseOffset);
+          if(roundCoords) { dragStart = dragStart.map(Math.round); }
+          
+          if(callbackStart) {
+              callbackStart(dragged, dragStart);
+          }
+      }
+  }
+
+  function onMove(e) {
+      if(!dragged) { return; }
+      e.preventDefault();
+
+      const pos = getMousePos(e, container, mouseOffset, !dragOutside);
+      callback(dragged, pos, dragStart);
+  }
+
+  function onEnd(e) {
+      if(!dragged) { return; }
+
+      if(callbackEnd) {
+          const pos = getMousePos(e, container, mouseOffset, !dragOutside);
+          callbackEnd(dragged, pos, dragStart);
+      }
+      dragged = null;
+  }
+
+  /* Mouse/touch input */
+
+  container.addEventListener('mousedown', function(e) {
+      if(isLeftButton(e)) { onDown(e); }
+  });
+  container.addEventListener('touchstart', function(e) {
+      onDown(tweakTouch(e));
+  });
+
+  window.addEventListener('mousemove', function(e) {
+      if(!dragged) { return; }
+
+      if(isLeftButton(e)) { onMove(e); }
+      //"mouseup" outside of window
+      else { onEnd(e); }
+  });
+  window.addEventListener('touchmove', function(e) {
+      onMove(tweakTouch(e));
+  });
+
+  container.addEventListener('mouseup', function(e) {
+      //Here we check that the left button is *no longer* pressed:
+      if(!isLeftButton(e)) { onEnd(e); }
+  });
+  function onTouchEnd(e) { onEnd(tweakTouch(e)); }
+  container.addEventListener('touchend', onTouchEnd);
+  container.addEventListener('touchcancel', onTouchEnd);
+
+
+  function isLeftButton(e) {
+      return (e.buttons !== undefined)
+          ? (e.buttons === 1)
+          //Safari (not tested):
+          //https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent#Browser_compatibility
+          : (e.which === 1);
+  }
+  function tweakTouch(e) {
+      let touch = e.targetTouches[0];
+      //touchend:
+      if(!touch) { touch = e.changedTouches[0]; }
+      
+      touch.preventDefault = e.preventDefault.bind(e);
+      return touch;
+  }
+}
+
+/////////////////
+
 let accuracy = 5,
     clothX = 60,
     clothY = 25,
@@ -32,6 +294,12 @@ class Point {
   constructor(x, y) {
     this.x = x;
     this.y = y;
+
+    // if(poses[0] > 0) {
+    //   this.x = poses[0].pose.keypoints[9].position.x;
+    //   this.y = poses[0].pose.keypoints[9].position.y;
+    // }
+    
     this.px = x;
     this.py = y;
     this.vx = 0;
@@ -202,6 +470,8 @@ dragTracker({
     mouse.down = true;
     mouse.px = mouse.x = pos[0];
     mouse.py = mouse.y = pos[1];
+    // console.log('pose[0]: ', pos[0])
+    // console.log('pose[1]: ', pos[1])
   },
   callback: (_, pos) => {
     mouse.px = mouse.x;
@@ -245,6 +515,7 @@ function reset() {
 
 
 ////////////////////////////////////////////////////////////////
+
 
 
 // window.requestAnimFrame =
